56a0822e4e93a6cae68e97af60ab885589d5bb7a
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2025-01-25 23:27:46 -0500

    Fix import

--- src/server/context.ts
+++ src/server/context.ts
@@ -1,6 +1,7 @@
-import { inferAsyncReturnType } from '@trpc/server';
-import { CreateNextContextOptions } from '@trpc/server/adapters/next';
-import db from '@/db';
+import { inferAsyncReturnType } from '@trpc/server'
+import { CreateNextContextOptions } from '@trpc/server/adapters/next'
+
+import { db } from '@/db'
 
 export async function createContext(opts: CreateNextContextOptions) {
   return {
@@ -8,7 +9,7 @@ export async function createContext(opts: CreateNextContextOptions) {
     // Add any other context items you need
     req: opts.req,
     res: opts.res,
-  };
+  }
 }
 
-export type Context = inferAsyncReturnType<typeof createContext>;
+export type Context = inferAsyncReturnType<typeof createContext>
