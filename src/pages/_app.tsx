a19ae59e2af4627ae41ce1c1724c3f0da88b47e8
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2025-01-25 19:05:51 -0500

    Add SIWE

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,32 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit'
+import { RainbowKitSiweNextAuthProvider } from '@rainbow-me/rainbowkit-siwe-next-auth'
+import '@rainbow-me/rainbowkit/styles.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import type { Session } from 'next-auth'
+import { SessionProvider } from 'next-auth/react'
+import { AppProps } from 'next/app'
+import { WagmiProvider } from 'wagmi'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import { wagmiConfig } from '@/lib/web3'
+
+const queryClient = new QueryClient()
+
+type Props = AppProps<{
+  session: Session
+}>
+
+export default function App({ Component, pageProps }: Props) {
+  return (
+    <WagmiProvider config={wagmiConfig}>
+      <SessionProvider refetchInterval={0} session={pageProps.session}>
+        <QueryClientProvider client={queryClient}>
+          <RainbowKitSiweNextAuthProvider>
+            <RainbowKitProvider modalSize="compact">
+              <Component {...pageProps} />
+            </RainbowKitProvider>
+          </RainbowKitSiweNextAuthProvider>
+        </QueryClientProvider>
+      </SessionProvider>
+    </WagmiProvider>
+  )
 }
