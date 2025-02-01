5174df0c8dd2b80b0cf30591594c88b55a602f80
aslobodnik <aslobodnik@gmail.com>
2025-01-31 22:13:30 -0500

    Improve safe selector

--- src/components/SafeSelector.tsx
+++ src/components/SafeSelector.tsx
@@ -1,3 +1,10 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
 import { api } from '@/utils/trpc'
 
 interface SafeSelectorProps {
@@ -12,26 +19,28 @@ export default function SafeSelector({
   const { data: safes, isLoading } = api.safes.getAllSafesWithEns.useQuery()
 
   return (
-    <div>
-      <select
-        value={safeAddress || ''}
-        onChange={(e) => onChange(e.target.value)}
-        className="min-w-[300px] rounded-md border p-2 text-black"
-      >
-        <option value="">Select a Safe</option>
+    <Select
+      value={safeAddress === null ? 'all' : safeAddress}
+      onValueChange={onChange}
+    >
+      <SelectTrigger className="min-w-[300px] bg-white text-lg font-bold">
+        <SelectValue placeholder="All Safes" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value="all">All Safes</SelectItem>
         {isLoading && (
-          <option value="" disabled>
+          <SelectItem value="loading" disabled>
             Loading safes...
-          </option>
+          </SelectItem>
         )}
         {safes?.map((safe) => (
-          <option key={safe.address} value={safe.address}>
+          <SelectItem key={safe.address} value={safe.address}>
             {safe.name
               ? safe.name
               : `${safe.address.slice(0, 6)}...${safe.address.slice(-4)}`}
-          </option>
+          </SelectItem>
         ))}
-      </select>
-    </div>
+      </SelectContent>
+    </Select>
   )
 }
