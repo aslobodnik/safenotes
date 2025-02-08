eb94137b5ae5467a6c833cfae321ecb55bee8aa2
mendeleden <43044887+mendeleden@users.noreply.github.com>
2025-02-08 10:54:03 -0500

    add logs to inspect load times (#29)

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -112,19 +112,22 @@ export const createTRPCRouter = t.router
  * You can remove this if you don't like it, but it can help catch unwanted waterfalls by simulating
  * network latency that would occur in production but not in local development.
  */
-const timingMiddleware = t.middleware(async ({ next, path }) => {
+const timingMiddleware = t.middleware(async ({ next, path, type, input }) => {
   const start = Date.now()
-
-  if (t._config.isDev) {
-    // artificial delay in dev
-    const waitMs = Math.floor(Math.random() * 400) + 100
-    await new Promise((resolve) => setTimeout(resolve, waitMs))
-  }
-
   const result = await next()
-
   const end = Date.now()
-  console.log(`[TRPC] ${path} took ${end - start}ms to execute`)
+  const duration = end - start
+
+  console.log(
+    JSON.stringify({
+      timestamp: new Date().toISOString(),
+      path,
+      type,
+      input,
+      duration_ms: duration,
+      message: `[TRPC] ${path} ${type} took ${duration}ms`,
+    })
+  )
 
   return result
 })
